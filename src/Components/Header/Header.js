dc9c14ee5e200ded6d339114c9b5c9f743ad6153
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-02-10 17:32:27 +0400

    Add styles.js for Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,57 @@
-// import SvgIcon from '@material-ui/core/SvgIcon';
-// import React from 'react';
-//
-// function HomeIcon(props) {
-//   return (
-//     <SvgIcon {...props}>
-//       <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-//     </SvgIcon>
-//   );
-// }
-//
-// const styles = {
-//   toolbar: {
-//     display: 'flex',
-//     justifyContent: 'flex-start'
-//   },
-//   logo: {
-//     color: 'white',
-//     textDecoration: 'none'
-//   },
-//   loginBtn: {
-//     marginLeft: 'auto',
-//     textDecoration: 'none'
-//   }
-// };
-//
-// export default [HomeIcon, styles];
-//
-// // import  HomeIcon  from './Header';
-// // import   styles  from './Header';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import HomeIcon from './HomeIcon';
+import { styles } from './styles';
+
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+const VARIABLES = {
+  home: 'Blog',
+  createPost: 'Create Post',
+  loginButtonLabel: 'Log in',
+  logoutButtonLabel: 'Log out'
+};
+
+function Header(props) {
+  const { classes, isLoggedIn } = props;
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Link to="/" className={classes.logo}>
+          <IconButton edge="start" color="inherit" aria-label="blog">
+            <HomeIcon fontSize="large" />
+            <Typography variant="h4">{VARIABLES.home}</Typography>
+          </IconButton>
+        </Link>
+        <Button className="{styles.createPostBtn}" color="inherit" size="large">
+          {VARIABLES.createPost}
+        </Button>
+        <Link to="/auth" className={classes.loginBtn}>
+          {isLoggedIn ? (
+            <Button style={{ color: 'white' }} size="large">
+              {VARIABLES.logoutButtonLabel}
+            </Button>
+          ) : (
+            <Button style={{ color: 'white' }} size="large">
+              {VARIABLES.loginButtonLabel}
+            </Button>
+          )}
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Header);
---
+++ src/Components/Header/styles.js
@@ -0,0 +1,14 @@
+export const styles = {
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-start'
+  },
+  logo: {
+    color: 'white',
+    textDecoration: 'none'
+  },
+  loginBtn: {
+    marginLeft: 'auto',
+    textDecoration: 'none'
+  }
+};
