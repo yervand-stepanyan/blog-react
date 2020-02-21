f5bb8927217053b479548c2891dad16f2efed38f
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-02-21 21:15:46 +0400

    Updates

---
+++ precache-manifest.38ef0b0a42a92b14ebd594499cd0dd1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22420723bc029c2e9cce7952e237c41b",
+    "url": "/blog-react/index.html"
+  },
+  {
+    "revision": "7d24afb0836d8e4e9890",
+    "url": "/blog-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a26b57ad0b1a06e239ee",
+    "url": "/blog-react/static/js/2.fc2da6da.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/blog-react/static/js/2.fc2da6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d24afb0836d8e4e9890",
+    "url": "/blog-react/static/js/main.69e6d637.chunk.js"
+  },
+  {
+    "revision": "e1b750370b98cefde77e",
+    "url": "/blog-react/static/js/runtime-main.36bfefa8.js"
+  }
+]);
